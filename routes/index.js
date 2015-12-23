1d11491fb20f6c8d376dbcb85fdda5beb66a7b22
Rômulo A. Ceccon <romuloceccon@gmail.com>
2015-12-23 21:57:02 -0200

    Add basic file serving route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var path = require('path');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/*', function(req, res, next) {
+  var f = path.normalize(__dirname + '/../files' + req.path);
+  res.sendFile(f);
 });
 
 module.exports = router;
